da6c1f490826feaa06c8866998b5c0855b4258a8
Pierce Hokia <pierce_zachary_hokia@dlsu.edu.ph>
2025-08-02 14:07:18 +0800

    finished full functionality for saved quotes

--- app/src/main/java/com/example/solofit/QuoteActivities/ViewSavedQuote.kt
+++ app/src/main/java/com/example/solofit/QuoteActivities/ViewSavedQuote.kt
@@ -1,15 +1,20 @@
 package com.example.solofit.QuoteActivities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.solofit.R
 import com.example.solofit.database.MyDatabaseHelper
-import com.example.solofit.databinding.QuestInfoBinding
 import com.example.solofit.databinding.ViewSavedQuoteBinding
 import com.example.solofit.model.Quote
 import com.example.solofit.utilities.Extras
 
 class ViewSavedQuote : AppCompatActivity(){
+    companion object {
+        const val CONFIRM_DELETE_MESSAGE = "Are you sure you want to delete this from your quotes?"
+        const val YES = "Yes"
+        const val NO = "No"
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewBinding: ViewSavedQuoteBinding = ViewSavedQuoteBinding.inflate(layoutInflater)
@@ -26,15 +31,29 @@ class ViewSavedQuote : AppCompatActivity(){
         }
         viewBinding.tvQuoteContent.text = quote.quoteText
         viewBinding.tvQuoteAuthor.text = quote.quoteAuthor
-//        viewBinding.ivSaveQuote.setOnClickListener {
-//            isSaved = !isSaved
-//            val updatedIcon = if (isSaved) R.drawable.icon_saved_quote else R.drawable.icon_save_quote
-//            viewBinding.imbSaveQuote.setImageResource(updatedIcon)
-//
-//            selectedQuote?.let { quote ->
-//                dbHelper.updateQuoteSaveStatus(quote.quoteID, isSaved)
-//            }
-//        }
+        viewBinding.ivSaveQuote.setOnClickListener {
+            viewBinding.txvConfirmationMsg.text = CONFIRM_DELETE_MESSAGE
+            viewBinding.btnConfirm.text = YES
+            viewBinding.btnGoBack.text = NO
+            handleConfirmation(viewBinding)
+        }
+        viewBinding.btnConfirm.setOnClickListener {
+            quote.isSaved = !quote.isSaved
+            dbHelper.updateQuote(quote)
+            finish()
+        }
+        viewBinding.btnGoBack.setOnClickListener {
+            hidePanel(viewBinding)
+        }
+    }
+    private fun handleConfirmation(viewBinding: ViewSavedQuoteBinding) {
+        viewBinding.cloConfirmation.visibility = View.VISIBLE
+        viewBinding.viewBackgroundBlocker.visibility = View.VISIBLE
+    }
+    private fun hidePanel(viewBinding: ViewSavedQuoteBinding)
+    {
+        viewBinding.cloConfirmation.visibility = View.INVISIBLE
+        viewBinding.viewBackgroundBlocker.visibility = View.INVISIBLE
     }
 
 }
